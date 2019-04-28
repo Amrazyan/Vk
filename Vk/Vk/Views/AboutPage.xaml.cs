fb164ddd9fd10d2a714300faff2f8567df0e61c4
Amrazyan <amrazyan@gmail.com>
2019-04-28 13:03:03 +0400

    Adding project

---
+++ Vk/Vk/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vk.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
