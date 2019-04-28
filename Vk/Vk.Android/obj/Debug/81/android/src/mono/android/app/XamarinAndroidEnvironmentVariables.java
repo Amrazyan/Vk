ea0700ff88d3a7aa799ff6a5ce7de6c58b49eb3f
Amrazyan <amrazyan@gmail.com>
2019-04-28 15:06:58 +0400

    button styles

--- Vk/Vk.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Vk/Vk.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "d0a1ece7-e3b9-474a-9e35-2a9211e3f3e5",
+		"XAMARIN_BUILD_ID", "21d10cef-cd16-4ad3-b10a-6c57f6681222",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
